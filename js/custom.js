2546fd1ec2842c534761c6d45b284f35ff155e48
KennedyFVB <kenkahura@gmail.com>
2022-11-01 18:29:51 +0300

    add toggle effect to what we do section

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
     $("#design_icon").click(function() {
         $("#design").toggle();
-    })
-})
+    });
+
+    $("#development_icon").click(function() {
+        $("#development").toggle();
+    });
+
+    $("#product_management_icon").click(function() {
+        $("#product_management").toggle();
+    });
+});
+
+// $(document).ready(function() {
+//     $("#development_icon").click(function() {
+//         $("#development").toggle();
+//     })
+// })
