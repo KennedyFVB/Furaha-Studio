ab47b2103d45d984557e7814426a1f1959194d84
KennedyFVB <kenkahura@gmail.com>
2022-11-02 21:14:15 +0300

    added hover and improved contact us section

--- js/custom.js
+++ js/custom.js
@@ -10,10 +10,22 @@ $(document).ready(function() {
     $("#product_management_icon").click(function() {
         $("#product_management").toggle();
     });
+
+    // $("#work1-image").mousemove(function() {
+    //     $("#work1-hover").hover();
+    // });
+
+
+    // $("#work1-image").mouseenter(function () {
+    //     $(this).hide();
+    //     $("#work1-hover").show();
+    // });
+ 
+    // $("#work1-hover").mouseleave(function () {
+    //     $("#work1-image").show();
+    //     $(this).hide();
+    // }).mouseleave();//trigger mouseleave to hide second div in beginning 
+
 });
 
-// $(document).ready(function() {
-//     $("#development_icon").click(function() {
-//         $("#development").toggle();
-//     })
-// })
+
