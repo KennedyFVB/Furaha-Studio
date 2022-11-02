d1ac8054cc7e701dea4cacbbd60479420d99f30c
KennedyFVB <kenkahura@gmail.com>
2022-11-02 22:19:10 +0300

    updated

--- js/custom.js
+++ js/custom.js
@@ -28,14 +28,14 @@ $(document).ready(function() {
 
 });
 
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
-if (toastTrigger) {
-  toastTrigger.addEventListener('click', () => {
-    const toast = new bootstrap.Toast(toastLiveExample)
+// const toastTrigger = document.getElementById('liveToastBtn')
+// const toastLiveExample = document.getElementById('liveToast')
+// if (toastTrigger) {
+//   toastTrigger.addEventListener('click', () => {
+//     const toast = new bootstrap.Toast(toastLiveExample)
 
-    toast.show()
-  })
-}
+//     toast.show()
+//   })
+// }
 
 
